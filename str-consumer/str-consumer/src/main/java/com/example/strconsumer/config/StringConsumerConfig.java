191fa3afe1c694e9758b17c51fc8711af4acb670
gustavo.guimaraes <gustavosilvadky02@gmail.com>
2023-03-19 19:40:35 -0300

    consumer

---
+++ str-consumer/str-consumer/src/main/java/com/example/strconsumer/config/StringConsumerConfig.java
@@ -0,0 +1,38 @@
+package com.example.strconsumer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+
+@Configuration
+@RequiredArgsConstructor
+public class StringConsumerConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        HashMap<String, Object> configs = new HashMap<>();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configs);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> strContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory );
+        return factory;
+    }
+}
