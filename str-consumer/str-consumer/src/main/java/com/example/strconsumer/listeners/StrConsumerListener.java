98994253b51141a597fc5beab7ca44f1b4787a43
gustavo.guimaraes <gustavosilvadky02@gmail.com>
2023-03-23 21:09:54 -0300

    record interceptor

--- str-consumer/str-consumer/src/main/java/com/example/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/str-consumer/src/main/java/com/example/strconsumer/listeners/StrConsumerListener.java
@@ -20,7 +20,7 @@ public class StrConsumerListener {
         log.info("LOG ::: Receive message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void hist(String message) {
         log.info("HIST ::: Receive message {}", message);
     }
